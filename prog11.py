910b070e1a7315e51f8c4de5477d34f64aa938af
Student <student@TesterVM-38.edu-wsb.local>
2020-11-08 13:12:54 +0100

    prog11.py

---
+++ prog11.py
@@ -0,0 +1,6 @@
+zoo = ['zyrafa', 'slonie', 'malp']
+ilosc = [1, 3, 6]
+
+for idx in range(len(zoo)):
+    print("idx: " + str(idx) + ": " + zoo[idx])
+    print("Jest w zoo " + str(ilosc[idx]) + " " + zoo[idx])
